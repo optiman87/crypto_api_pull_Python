467601f9d5379911850c12bf6b7e8b22d4e8b5c4
optiman87 <36527043+optiman87@users.noreply.github.com>
2024-06-27 21:37:10 -0400

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,9 @@
 # Import the required libraries
 import pandas as pd
 
+# Set the float format to 2 decimals
+pd.options.display.float_format = '{:,.2f}'.format
+
 # Import the extract cvs file into a dataframe
 df= pd.read_csv('/Users/ek/Desktop/PythonPortfolio/api_extract.csv')
 
@@ -10,11 +13,19 @@ df= df.rename(columns={'name': 'Cryptocurrency', 'quote.CAD.price':'CAD Price','
 # Create a new dataframe to store the required columns
 df2= df[['Cryptocurrency', 'CAD Price', '1 hour', '24 hours', '7 days', '30 days', '60 days', '90 days']]
 
-# Group the dataframe by Cryptocurrency and display the mean CAD Price, 1 hour, 24 hours, 7 days, 30 days, 60 days, 90 days values
-df2= df2.groupby('Cryptocurrency')[['CAD Price', '1 hour', '24 hours', '7 days', '30 days', '60 days', '90 days']].mean()
+# Group the dataframe by Cryptocurrency and CAD Price and display the mean 1 hour, 24 hours, 7 days, 30 days, 60 days, 90 days values
+df2= df2.groupby(['Cryptocurrency','CAD Price'])[['1 hour', '24 hours', '7 days', '30 days', '60 days', '90 days']].mean()
+
+# Unpivot the columns in the dataframe and store in a new dataframe for visualization
+df3= df2.stack().reset_index()
+
+# Rename the columns in the new dataframe
+df3= df3.rename(columns= {'level_2': 'Time', 0: 'CAD Percent Change'})
+
+# Inspect the new dataframe
+df3.info()
 
 # Save the dataframe to a csv file
-df2.to_csv('/Users/ek/Desktop/PythonPortfolio/clean_data.csv')
-
+df3.to_csv('/Users/ek/Desktop/PythonPortfolio/clean_data.csv', index=False)
 
 
